273ef25e192533054a148aa9a833602582bed958
Cesar Junior <csrjr@pm.me>
2023-11-18 14:11:59 -0300

    Attach LiveData to AutocompleteTextView.

--- app/src/main/java/br/usp/ime/mac0447/datasetcollector/ui/main/MainFragment.kt
+++ app/src/main/java/br/usp/ime/mac0447/datasetcollector/ui/main/MainFragment.kt
@@ -6,8 +6,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import br.usp.ime.mac0447.datasetcollector.R
+import br.usp.ime.mac0447.datasetcollector.databinding.FragmentMainBinding
 
+@Suppress("DEPRECATION")
 class MainFragment : Fragment() {
 
     companion object {
@@ -15,6 +20,12 @@ class MainFragment : Fragment() {
     }
 
     private lateinit var viewModel: MainViewModel
+    private var _binding: FragmentMainBinding? = null
+    // This property is only valid between onCreateView and
+// onDestroyView.
+    private val binding get() = _binding!!
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,7 +37,23 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this)[MainViewModel::class.java]
+        viewModel.objTypes.observe(viewLifecycleOwner, Observer {
+            objTypes -> binding.actObjTypeName.setAdapter(ArrayAdapter(requireContext(), androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, objTypes))
+        })
+    }
 }
--- app/src/main/java/br/usp/ime/mac0447/datasetcollector/ui/main/MainViewModel.kt
+++ app/src/main/java/br/usp/ime/mac0447/datasetcollector/ui/main/MainViewModel.kt
@@ -1,7 +1,19 @@
 package br.usp.ime.mac0447.datasetcollector.ui.main
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import br.usp.ime.mac0447.datasetcollector.dto.ObjType
+import br.usp.ime.mac0447.datasetcollector.service.ObjTypeService
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    var objTypes: MutableLiveData<ArrayList<ObjType>> = MutableLiveData<ArrayList<ObjType>>()
+    var objTypeService: ObjTypeService = ObjTypeService()
+
+    init {
+        fetchObjTypes("e")
+    }
+
+    fun fetchObjTypes(typeName: String) {
+        objTypes = objTypeService.fetchObjTypes(typeName)
+    }
 }
