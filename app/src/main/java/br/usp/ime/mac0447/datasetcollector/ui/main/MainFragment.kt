b1cb7e1841fc9842aeea41b8ab4157d8558e04d3
Cesar Junior <csrjr@pm.me>
2023-11-18 16:12:05 -0300

    Add camera logic to take photo and save to external storage with custom file name.

--- app/src/main/java/br/usp/ime/mac0447/datasetcollector/ui/main/MainFragment.kt
+++ app/src/main/java/br/usp/ime/mac0447/datasetcollector/ui/main/MainFragment.kt
@@ -1,20 +1,38 @@
 package br.usp.ime.mac0447.datasetcollector.ui.main
 
+import android.annotation.SuppressLint
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.provider.MediaStore.Audio.Media
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import br.usp.ime.mac0447.datasetcollector.R
 import br.usp.ime.mac0447.datasetcollector.databinding.FragmentMainBinding
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Date
 
 @Suppress("DEPRECATION")
 class MainFragment : Fragment() {
 
+    private val SAVE_IMAGE_REQUEST_CODE: Int = 2122
+    private val CAMERA_REQUEST_CODE: Int = 2120
+    val CAMERA_PERMISSION_REQUEST_CODE: Int = 2121
+    private lateinit var currentPhotoPath: String
     companion object {
         fun newInstance() = MainFragment()
     }
@@ -55,5 +73,75 @@ class MainFragment : Fragment() {
         viewModel.objTypes.observe(viewLifecycleOwner, Observer {
             objTypes -> binding.actObjTypeName.setAdapter(ArrayAdapter(requireContext(), androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, objTypes))
         })
+        binding.btnAddPhoto.setOnClickListener {
+            prepTakePhoto()
+        }
+    }
+
+    private fun prepTakePhoto() {
+        if (ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            takePhoto()
+        } else {
+            val permissionRequest = arrayOf(android.Manifest.permission.CAMERA)
+            requestPermissions(permissionRequest, CAMERA_PERMISSION_REQUEST_CODE)
+        }
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            CAMERA_PERMISSION_REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    takePhoto()
+                } else {
+                    Toast.makeText(requireContext(), "Unable to take photo without permission", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+
+
+    private fun takePhoto() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also {
+            takePictureIntent -> takePictureIntent.resolveActivity(requireContext().packageManager)
+            if (takePictureIntent == null) {
+                Toast.makeText(requireContext(), "Unable to save photo", Toast.LENGTH_LONG).show()
+            } else {
+                val photoFile: File = createImageFile()
+                photoFile?.also {
+                    val photoUri = FileProvider.getUriForFile(requireActivity().applicationContext, "br.usp.ime.mac0447.datasetcollector.android.fileprovider", it)
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoFile)
+                    startActivityForResult(takePictureIntent, SAVE_IMAGE_REQUEST_CODE)
+                }
+            }
+        }
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK) {
+            if (requestCode == CAMERA_REQUEST_CODE) {
+                val imageBitmap = data!!.extras!!.get("data") as Bitmap
+                binding.imgObjType.setImageBitmap(imageBitmap)
+            } else if (requestCode == SAVE_IMAGE_REQUEST_CODE) {
+                Toast.makeText(requireContext(), "Image saved", Toast.LENGTH_LONG).show()
+
+            }
+        }
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    private fun createImageFile(): File {
+        val timestamp: String = SimpleDateFormat("yyyyMMdd_HHmmsss").format(Date())
+        val storageDir: File? = requireContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile("Dataset${timestamp}", ".jpg", storageDir).apply {
+            currentPhotoPath = absolutePath
+        }
     }
 }
