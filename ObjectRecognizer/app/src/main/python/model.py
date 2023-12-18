295be45aed0cf76f67fccd326c5db291e763fdbc
Cesar Junior <csrjr@pm.me>
2023-12-18 01:09:45 -0300

    Update dataset and classifier model.

--- ObjectRecognizer/app/src/main/python/model.py
+++ ObjectRecognizer/app/src/main/python/model.py
@@ -8,36 +8,72 @@ from PIL import Image
 
 from os.path import dirname, join
 
-from sklearn.linear_model import SGDClassifier
+from sklearn.tree import DecisionTreeClassifier
 
 H = 256
 
-filename = join(dirname(__file__), "sgd_clf.joblib")
+filename = join(dirname(__file__), "clf.joblib")
 clf = joblib.load(filename)
 
-def get_edges(img):
-    return cv2.Canny(img, 20, 230)
+clahe = cv2.createCLAHE()
 
-def get_seg(edges):
-    new_img = np.zeros_like(edges)
-    contour, _ = cv2.findContours(edges, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+
+def normalize(img):
+    return clahe.apply(img)
+
+
+def binarize(img):
+    _, thresh = cv2.threshold(cv2.GaussianBlur(img, (5,5), 0), 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    return thresh
+
+
+def get_edges(img):
+    blur = cv2.GaussianBlur(img, (3,3), 0)
+    return cv2.Canny(blur, 20, 255)
+
+
+def get_seg(img):
+    contour, _ = cv2.findContours(img, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
 
     for cnt in contour:
-        cv2.drawContours(new_img, [cnt], 0, 255, -1)
+        cv2.drawContours(img, [cnt], 0, 255, -1)
 
-    rect = cv2.getStructuringElement(cv2.MORPH_RECT, (4, 4))
-    dilation = cv2.dilate(new_img, rect, iterations=5)
-    erosion = cv2.erode(dilation, rect,  iterations=4)
-    dilation = cv2.dilate(erosion, rect, iterations=5)
-    erosion = cv2.erode(dilation, rect,  iterations=4)
+    rect = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+    dilation = cv2.dilate(img, rect, iterations=2)
+    closing = cv2.morphologyEx(dilation, cv2.MORPH_CLOSE, rect)
 
-    #cnt_img = np.zeros_like(erosion)
-    #contour, _ = cv2.findContours(erosion, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+    final_cnts, _ = cv2.findContours(closing, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+    cnt = max(final_cnts, key=cv2.contourArea)
 
-    #cnt = max(contour, key=cv2.contourArea)
-    #cv2.drawContours(cnt_img, [cnt], 0, 255, -1)
+    seg = np.zeros_like(img)
+    cv2.drawContours(seg, [cnt], 0, 255, -1)
 
-    return erosion
+    return seg, cnt
+
+
+def get_features(cnt):
+    area = cv2.contourArea(cnt)
+
+    M = cv2.moments(cnt)
+    cx = int(M["m10"] / M["m00"])
+    cy = int(M["m01"] / M["m00"])
+
+    rect = cv2.minAreaRect(cnt)
+    box = cv2.boxPoints(rect)
+    box = np.intp(box)
+    area_norm_box = area/cv2.contourArea(box)
+
+    (x,y), r = cv2.minEnclosingCircle(cnt)
+    area_norm_circle = area/(np.pi * r**2)
+
+    cx_norm = abs(cx - x)
+    cy_norm = abs(cy - y)
+
+    w, h = rect[1]
+    stretch = max(w/h, h/w)
+
+    return area_norm_box, area_norm_circle, cx_norm, cy_norm, stretch
+
 
 def get_feret_box(img_seg, ratio):
     x_in, y_in =np.where(img_seg == 255)
@@ -47,6 +83,7 @@ def get_feret_box(img_seg, ratio):
 
     return box_min, box_max
 
+
 def draw_feret_box(img, box_min, box_max, txt):
     img_copy = img.copy()
 
@@ -55,6 +92,7 @@ def draw_feret_box(img, box_min, box_max, txt):
 
     return img_copy
 
+
 def detect_and_draw(img_str):
     decoded_data = base64.b64decode(img_str)
     np_data = np.fromstring(decoded_data, np.uint8)
@@ -62,10 +100,13 @@ def detect_and_draw(img_str):
 
     input_img_resz = cv2.resize(input_img, (H,H))
     img = cv2.cvtColor(input_img_resz, cv2.COLOR_BGR2GRAY)
-    edges = get_edges(img)
-    seg = get_seg(edges)
 
-    pred = clf.predict([seg.flatten()])[0]
+    edges = get_edges(img)
+    seg, cnt = get_seg(edges)
+
+    feats = get_features(cnt)
+
+    pred = clf.predict([feats])[0]
 
     ratio = np.round(input_img.shape[0] / H)
     box_min, box_max = get_feret_box(seg, 1)
